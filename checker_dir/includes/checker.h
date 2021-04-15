70f785f0a13c8b18f821d1cfe422983b47b83d58
hayaks <julien.sag36@gmail.com>
2021-04-15 15:46:47 +0200

    wesh

--- checker_dir/includes/checker.h
+++ checker_dir/includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: jsaguez <jsaguez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 15:59:57 by jsaguez           #+#    #+#             */
-/*   Updated: 2021/04/14 15:08:03 by jsaguez          ###   ########.fr       */
+/*   Updated: 2021/04/15 15:22:27 by jsaguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,6 @@ long int		ft_atoi_verif(const char *nbr);
 t_push  		*ft_malloc_push(int ac, char **av);
 void			ft_free_push(t_push *push);
 void			ft_begin_pile(t_push *push, char **av);
+void			ft_get_instruction(t_push *push);
+void			ft_get_instruction_bis(t_push *push, char *line);
 #endif
--- checker_dir/srcs/main.c
+++ checker_dir/srcs/main.c
@@ -6,11 +6,39 @@
 /*   By: jsaguez <jsaguez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 23:56:04 by jsaguez           #+#    #+#             */
-/*   Updated: 2021/04/15 14:59:01 by jsaguez          ###   ########.fr       */
+/*   Updated: 2021/04/15 15:30:38 by jsaguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/checker.h"
+
+void	ft_get_instruction_bis(t_push *push, char *line)
+{
+	if (ft_strcmp(line, "rb") == 0)
+		rb;
+	else if (ft_strcmp(line, "rr") == 0)
+	{
+		ra;
+		rb;
+	}
+	else if (ft_strcmp(line, "rra") == 0)
+		rra;
+	else if (ft_strcmp(line, "rrb") == 0)
+		rrb;
+	else if (ft_strcmp(line, "rrr") == 0)
+	{
+		rra;
+		rrb;
+	}
+	else
+	{
+		ft_putstr_fd("Error\n", 2);
+		ft_free_push(push);
+		free(line);
+		line = NULL;
+		exit(1);
+	}
+}
 
 void	ft_get_instruction(t_push *push)
 {
@@ -19,6 +47,23 @@ void	ft_get_instruction(t_push *push)
 	line = NULL;
 	while (get_next_line(0, &line) > 0)
 	{
+		if (ft_strcmp(line, "sa") == 0)
+			sa;
+		else if (ft_strcmp(line, "sb") == 0)
+			sb;
+		else if (ft_strcmp(line, "ss") == 0)
+		{
+			sa;
+			sb;
+		}
+		else if (ft_strcmp(line, "pa") == 0)
+			pa;
+		else if (ft_strcmp(line, "pb") == 0)
+			pb;
+		else if (ft_strcmp(line, "ra") == 0)
+			ra;
+		else
+			ft_get_instruction_bis(push, line);
 		free(line);
 		line = NULL;
 	}
